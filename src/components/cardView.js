982da6f95accbca0f710a6ef9f60c49058acd2bf
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2020-09-26 02:40:11 +0500

    dsfsd

--- src/components/cardView.js
+++ src/components/cardView.js
@@ -1,15 +1,41 @@
-import { Box } from '@material-ui/core';
+import { makeStyles, Paper } from '@material-ui/core';
 import React from 'react'
+import image from "../temp/1_1.jpg";
 
+const temp_data = {
+    img : image,
+    name : "BG Raxer 14",
+    price : 0,
+}
+const useStyle = makeStyles(
+    (theme) =>(
+        {
+            paper : {
+                width : "300px",
+                height : 'auto',
+                margin : '5%'
+            },
+            image : {
+                width : "300px",
+                height : "250px",
+            }
+        }
+    )
+)
 
 function CardView()
 {
 
+    const classes = useStyle();
+
     return(
-        <Box>
-            <h1>Haider</h1>
-            <h3>haider</h3>
-        </Box>
+        <Paper
+        className = {classes.paper}
+        >
+            <img className={classes.image} src={temp_data['img']} />
+            <h3>{temp_data['name']}</h3>
+            <p> {temp_data['price']} </p>
+        </Paper>
     )
 }
 
