a0d4f0beb68d403be8bfc6fd296d56d4d1ce7606
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2020-09-26 23:34:26 +0500

    gg

--- src/components/cardView.js
+++ src/components/cardView.js
@@ -1,42 +1,47 @@
-import { makeStyles, Paper } from '@material-ui/core';
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import image from "../temp/1_1.jpg";
 
-const temp_data = {
-    img : image,
-    name : "BG Raxer 14",
-    price : 0,
+const dummyData = {
+    image : image,
+    name : "Bio 21",
+    price : "$130"
 }
-const useStyle = makeStyles(
-    (theme) =>(
-        {
-            paper : {
-                width : "300px",
-                height : 'auto',
-                margin : '5%'
-            },
-            image : {
-                width : "300px",
-                height : "250px",
-            }
-        }
-    )
-)
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
 
-function CardView()
-{
+export default function ImgMediaCard() {
+  const classes = useStyles();
 
-    const classes = useStyle();
-
-    return(
-        <Paper
-        className = {classes.paper}
-        >
-            <img className={classes.image} src={temp_data['img']} />
-            <h3>{temp_data['name']}</h3>
-            <p> {temp_data['price']} </p>
-        </Paper>
-    )
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="200"
+          image={ dummyData['image'] }
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            { dummyData['name'] }
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            { dummyData['price'] }
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
-
-export default CardView;
---
+++ src/components/gridItems.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+function MyGridItems()
+{
+
+    
+}
+
+export default MyGridItems;
